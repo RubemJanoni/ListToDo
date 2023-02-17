73898cdf9e5331d6da622bff104dc8910adf6797
Rubem Janoni <rubemjanoni@gmail.com>
2023-02-17 19:46:13 +0000

    Add the main elements to script file

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+//Elements
+const input = document.querySelector('input'); //Captures the input element.
+const list = document.querySelector('ul'); //Captures the list element.
+const btn1 = document.querySelector('.btn1'); //Captures the btn element.
