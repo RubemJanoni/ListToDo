23684f7399c2b8b17bbbaba71371f7a0723bce7f
Rubem Janoni <rubemjanoni@gmail.com>
2023-02-17 19:47:35 +0000

    Add events

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,7 @@
 const input = document.querySelector('input'); //Captures the input element.
 const list = document.querySelector('ul'); //Captures the list element.
 const btn1 = document.querySelector('.btn1'); //Captures the btn element.
+
+//Events
+input.addEventListener('keyup', handleKeyUp); // Add a keyboard event 'keyup' and call the function 'handleKeyUp'
+btn1.addEventListener('click', reset); // Add a click event 'click' and call the function 'reset'
